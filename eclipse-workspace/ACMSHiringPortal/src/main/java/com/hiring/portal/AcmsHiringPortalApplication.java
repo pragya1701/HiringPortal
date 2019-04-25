9027a596835da5c72a371fc014b71d14e5e21fea
pragya1701 <42355299+pragya1701@users.noreply.github.com>
2019-04-26 02:35:47 +0530

    Update AcmsHiringPortalApplication.java

--- eclipse-workspace/ACMSHiringPortal/src/main/java/com/hiring/portal/AcmsHiringPortalApplication.java
+++ eclipse-workspace/ACMSHiringPortal/src/main/java/com/hiring/portal/AcmsHiringPortalApplication.java
@@ -1,16 +1,32 @@
 package com.hiring.portal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.hiring.portal.model.Coordinator;
+import com.hiring.portal.service.IAPIService;
+
 @SpringBootApplication
 @EnableJpaAuditing
-public class AcmsHiringPortalApplication extends SpringBootServletInitializer{
+public class AcmsHiringPortalApplication extends SpringBootServletInitializer implements CommandLineRunner{
 	public static void main(String[] args) {
-//		new File(APIController.uploadDirectory).mkdir();
+
 		SpringApplication.run(AcmsHiringPortalApplication.class, args);
 	}
+	@Autowired
+	IAPIService apiservice;
+	@Override
+	public void run(String... args) throws Exception{
+		
+		Coordinator cord = new Coordinator();
+		cord.setUsername("Test");
+		cord.setPassword("Test");
+		cord.setPhone("9033312198");
+		apiservice.doRegister(cord);
+		System.out.println("Admin Logged in");
+	}
 }
-
